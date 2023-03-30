d86fc94f744cca74b4f5c727202b65462a63ec2f
davetayl <dave@imagetaylor.com>
2023-03-30 21:22:37 +1100

    set up config file

---
+++ main.py
@@ -0,0 +1,17 @@
+import network
+import request
+import sys
+import os
+
+
+def main():
+    while True:
+        # Pull the API value
+    ...
+
+def relay():
+    ...
+
+
+if __name__ == "__main__":
+    main()
