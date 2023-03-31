bc606747817d805118f26aa7575c37181e7b1d08
davetayl <dave@imagetaylor.com>
2023-03-31 18:58:03 +1100

    testing api calls

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
 import json
+import requests
+import sys
 
+# Pull the config
 with open('config.json') as fh:
     config = json.load(fh)
-    print(config)
+    
 
-print('-'*20)
-print(config["operation"]["poll_int"])
+    
+# Create the api object
+def main():
+    print(get_api_key(config))
+
+
+def get_api_key(config):
+    payload = [
+        {
+            "cmd":"Login",
+            "param":{
+                "User":{
+                    "Version":"0",
+                    "userName":config["reolink"]["nvr_un"],
+                    "password":config["reolink"]["nvr_pw"]
+                }
+            }
+        }
+    ]
+    return requests.post(f'http://{config["reolink"]["nvr_ip"]}/api.cgi?cmd=Login', json=payload).json()[0]["value"]["Token"]["name"]
+
+if __name__ == "__main__":
+    main()
