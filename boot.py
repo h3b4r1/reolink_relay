90500e484e85a17cf0b1604b00be79b76a6e52c0
davetayl <dtaylor@silvacloud.net.au>
2023-04-01 14:02:30 +1100

    set up boot file

--- boot.py
+++ boot.py
@@ -1,2 +1,13 @@
 import network
+import json
 
+# Load config
+with open('config.json') as fh:
+    config = json.load(fh)
+
+constate = False
+while not constate:
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect("config["network"]["ssid"]", "config["network"]["secret"]")
+    constate = sta_if.isconnected()
